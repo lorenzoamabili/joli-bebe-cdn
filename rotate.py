06f3744b4bf4f9b65ef8f228383f2c77b5ababe0
lorenzoamabili <lorenzo.amabili@protonmail.com>
2026-03-29 18:24:02 +0200

    inverno estivo

--- rotate.py
+++ rotate.py
@@ -10,6 +10,6 @@ extensions = [".jpg", ".jpeg", ".png", ".webp", ".gif"]
 for img_path in image_folder.iterdir():
     if img_path.suffix.lower() in extensions:
         with Image.open(img_path) as img:
-            rotated = img.rotate(90, expand=True)  # 90° counter-clockwise
+            rotated = img.rotate(180, expand=True)  # 180°
             rotated.save(img_path)
         print(f"Rotated {img_path.name}")
